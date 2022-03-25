943424cd4f6a2bcab1f9d02cb3b0d818c482dec9
M. Cavalcante <mferreira.taking@pedepronto.com.br>
2022-03-25 16:36:42 -0300

    feat: Header da tela principal

--- lib/design_system/components/layouts/main.layout.dart
+++ lib/design_system/components/layouts/main.layout.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sbf/design_system/components/ui/app_input.dart';
+import 'package:flutter_sbf/design_system/components/ui/input_decoration.dart';
+import 'package:flutter_sbf/design_system/values/colors.dart';
 
 class MainLayout extends StatelessWidget {
   final bool activeBack;
@@ -13,7 +16,47 @@ class MainLayout extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Container(),
+          child: Column(
+            children: [
+              Container(
+                color: DefaultColors.brand,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(child: Image.network('https://via.placeholder.com/140x20')),
+                        Container(
+                          margin: const EdgeInsets.only(right: 18),
+                          child: const Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 16, right: 16, left: 16),
+                      child: AppInput(
+                        decoration: inputDecoration(
+                          hint: 'Buscar por produtos',
+                          fillColor: Colors.white,
+                          suffixIcon: GestureDetector(
+                            onTap: () => print('asdf'),
+                            child: const Icon(
+                              Icons.search,
+                              color: DefaultColors.brand,
+                            ),
+                          ),
+                          radius: 2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
